cf731ad7ddd0f6bd38f39c34e671fa4938a920a1
ershad <ershad@gmail.com>
2018-08-28 15:03:03 +0600

    polishing

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBPDLN6Hemz2rrSmjT5H89LAaZbJz4_oi0',
+    authDomain: 'ng-fitness-tracker-c530f.firebaseapp.com',
+    databaseURL: 'https://ng-fitness-tracker-c530f.firebaseio.com',
+    projectId: 'ng-fitness-tracker-c530f',
+    storageBucket: 'ng-fitness-tracker-c530f.appspot.com',
+    messagingSenderId: '934552538207'
+  }
 };
